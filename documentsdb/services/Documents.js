f3ac4fcaa88eba4d5679856a0eaebbe024c415c4
Matteo Nardi <m.nardi@unibo.it>
2015-07-24 14:06:06 +0200

    support docx

--- documentsdb/services/Documents.js
+++ documentsdb/services/Documents.js
@@ -1,40 +1,40 @@
 /*
  * Copyright (c) 2014 - Copyright holders CIRSFID and Department of
  * Computer Science and Engineering of the University of Bologna
- * 
- * Authors: 
+ *
+ * Authors:
  * Monica Palmirani – CIRSFID of the University of Bologna
  * Fabio Vitali – Department of Computer Science and Engineering of the University of Bologna
  * Luca Cervone – CIRSFID of the University of Bologna
- * 
+ *
  * Permission is hereby granted to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The Software can be used by anyone for purposes without commercial gain,
  * including scientific, individual, and charity purposes. If it is used
  * for purposes having commercial gains, an agreement with the copyright
  * holders is required. The above copyright notice and this permission
  * notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * Except as contained in this notice, the name(s) of the above copyright
  * holders and authors shall not be used in advertising or otherwise to
  * promote the sale, use or other dealings in this Software without prior
  * written authorization.
- * 
+ *
  * The end-user documentation included with the redistribution, if any,
  * must include the following acknowledgment: "This product includes
  * software developed by University of Bologna (CIRSFID and Department of
- * Computer Science and Engineering) and its authors (Monica Palmirani, 
+ * Computer Science and Engineering) and its authors (Monica Palmirani,
  * Fabio Vitali, Luca Cervone)", in the same place and form as other
  * third-party acknowledgments. Alternatively, this acknowledgment may
  * appear in the software itself, in the same form and location as other
  * such third-party acknowledgments.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -86,7 +86,7 @@ function isAllowed(user, path) {
     if (user instanceof Error) return false;
     var allowedPaths = user.properties.folders;
     return allowedPaths.filter(function (allowedPath) {
-        return path.indexOf(allowedPath) == 0;
+        return path.indexOf(allowedPath) === 0;
     }).length > 0;
 }
 
@@ -95,8 +95,8 @@ function isAllowed(user, path) {
 router.get('*', function (req, res, next) {
     if (!req.file) return next();
 
-    if (req.file.match(/.doc$/) && req.headers['accept'] == 'text/html') {
-        var doc2xml = new DocToXml(); 
+    if (req.file.match(/.doc[x]$/) && req.headers.accept == 'text/html') {
+        var doc2xml = new DocToXml();
         backend_fs.getFile(doc2xml, req.dir, req.file, function (err) {
             if (err == 404) res.status(404).end();
             else if (err) next(err);
